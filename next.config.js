82188fd433efbcca32f7fcdb6f82ab3f96963c31
Dex778 <43555123+dex778@users.noreply.github.com>
2021-08-09 23:30:54 -0400

    app build

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// const withPlugins = require('next-compose-plugins')
+// const withImages = require('next-images')
+
+// const nextConfig = {
+//   images: {
+//     domains: ["links.papareact.com", "fakestoreapi.com", 'pngimg.com' ]
+//   }
+// }
+
+// module.exports = withPlugins([[withImages]], nextConfig)
+
+
+
+
+module.exports = {
+    images: {
+        domains: ["links.papareact.com", "fakestoreapi.com"],
+    },
+}
