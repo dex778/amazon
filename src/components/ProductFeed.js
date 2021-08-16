4010b7029998e1fb5ffd05b4da81d4f0ef333377
Dex778 <43555123+dex778@users.noreply.github.com>
2021-08-16 19:54:03 -0400

    Checked

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -14,6 +14,7 @@ function ProductFeed({ products }) {
                     description={description}
                     category={category}
                     image={image}
+                    
                 />
 
             ))}
